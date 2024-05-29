cba4e0d900a0ea7514a6be8efd422303c704f902
gazaza51 <matthewfitzgerald.gaza@benilde.edu.ph>
2024-05-29 20:11:58 +0800

    Create 07_string_slicing.py

---
+++ Lecture/week04/07_string_slicing.py
@@ -0,0 +1,10 @@
+school="De La Salle - College of Saint Benilde"
+
+print(school[14:])
+print(school[14:21])
+print(school[14:21:2])
+
+print(school[:11])
+print(school[0:11])
+
+print(school[::-1])
